1f0bc1a2a6bd7facdb0713c9de88015ade41f863
SeregaLi <kaprall13rus@gmail.com>
2022-11-15 00:17:13 +0300

    + редми

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = { "Hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer science" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
+
+int num = 3;
+
+void SortArray(string[] array, int num)
+{
+    System.Console.Write($"Массив отсортирован не более {num} символами:\t");
+    for (int i = 0; i < array.Length; i++)
+    {
+        bool IsStringNumeric(char charToCheck) => char.IsLetterOrDigit(charToCheck) || char.IsAscii(charToCheck);
+        int count = 0;
+        foreach (var el in array[i]) if (IsStringNumeric(el)) count++;
+        if (num >= count)
+        {
+            System.Console.Write("“");
+            System.Console.Write($"{array[i]}");
+            System.Console.Write("”\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+SortArray(arr1, num);
+SortArray(arr2, num);
+SortArray(arr3, num);
